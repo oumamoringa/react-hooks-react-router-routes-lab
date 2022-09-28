7b6fe75795e97723d0d3f118af30db5f7b7d9a4e
oumarichard <richard.ouma@student.moringaschool.com>
2022-09-28 19:22:06 +0300

    calling functions

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { directors } from "../data";
 
+function Directors() {
 
-const directorList= directors.map((dir)=>{
-  //console.log('math ' + Math.floor(Math.random()  * (1000 - 100)) + 100)
-  const moviesList = (dir.movies).map((mov)=><li key={Math.floor(Math.random()  * (1000 - 100)) + 100}>{mov}</li>)
-    return (
+  const directorList= directors.map((dir)=>{
+    //console.log('math ' + Math.floor(Math.random()  * (1000 - 100)) + 100)
+    const moviesList = (dir.movies).map((mov)=><li key={Math.floor(Math.random()  * (1000 - 100)) + 100}>{mov}</li>)
+      return (
+        <div>
+           <h1>{dir.name}</h1>
+           <ul>
+             {moviesList}
+           </ul>
+        </div>)
+  })
+
+  return (
       <div>
-         <h1>{dir.name}</h1>
-         <ul>
-           {moviesList}
-         </ul>
-      </div>)
-})
+        <h1>Directors Page</h1>
+        {directorList}
+      </div>
+  )
+}
 
-return (
-    <div>
-      <h1>Directors Page</h1>
-      {directorList}
-    </div>
-)
 export default Directors;
